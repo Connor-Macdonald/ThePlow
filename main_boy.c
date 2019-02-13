ff335aed85c090910e45d0aae08cb2c2efabfaa2
Garrett Richardson <garrett8716@hotmail.com>
2019-02-13 15:15:21 -0500

    Drive straight for distance

--- main_boy.c
+++ main_boy.c
@@ -23,11 +23,19 @@
 #define DIST_SENSOR_2 0x00000048
 
 
+
+struct Dist_sensor
+{
+   /* data */
+    volatile int *sideways_sensor;
+    volatile int *backwards_sensor;
+};
+
 void testSpeeds(volatile int *left_servo, volatile int *right_servo, volatile int *push_button){
 
-    //RESULTS 
+    //RESULTS
     // So far L:35 R:43 provides the straightest line
-    // right should be 8 bigger 
+    // right should be 8 bigger
     sleep(2);
     write_servo(70, left_servo, 1);
     write_servo(78, right_servo, 0);
@@ -69,12 +77,7 @@ void testSpeeds(volatile int *left_servo, volatile int *right_servo, volatile in
     write_servo(0, left_servo, 1);
     write_servo(0, right_servo, 0);
 }
-struct globalvariable 
-{
-   /* data */
-    int *xDtata = 0;
-    int *yData = 0;
-}; 
+
 /*
 compile command is gcc thread.c -thread
 
@@ -87,27 +90,16 @@ http://www.cse.cuhk.edu.hk/~ericlo/teaching/os/lab/9-PThread/Pass.html
 Random thread function for testing
 void* is used when working with different pointer types
 */
-void *sensor_thread(void *num){
-    //int *p = (int *)num;
-    int i =0;
-    //printf("im here in thread 1\n");
-    for(i =0;i<50;i++){
-        x = i;
-    printf("Thread 1 works! %d\n", x);
-    /*
-    while(1){
-        //int *xData = 0xff7300;
-        //int *yData = 0xff7310;
-        printf("Thread 1 works! %d\n", num);
-    }
-    */
-   sleep(1);
-    }
+void *sensor_thread(void *sensors){
+    struct Dist_sensor *thread_sensors = (struct Dist_sensor*)sensors;
+    printf("IN THREAD");
+    weighted_distance_sensor((*thread_sensors).sideways_sensor, (*thread_sensors).backwards_sensor);
 }
 
 
 int main(void)
 {
+    printf("START");
     // Initialization of FPGA bridge
     int fd = -1; // used to open /dev/mem
     void *LW_virtual; // physical addresses for light-weight bridge
@@ -118,31 +110,46 @@ int main(void)
     // Initialize all the nessacary virtual address pointers
 	volatile int * left_servo = (unsigned int *) (LW_virtual + LEFT_SERVO);
     volatile int * right_servo = (unsigned int *) (LW_virtual + RIGHT_SERVO);
-    volatile int * right_servo_encoder = (int *) (LW_virtual + RIGHT_SERVO_ENCODER); 
-    volatile int * left_servo_encoder = (int *) (LW_virtual + LEFT_SERVO_ENCODER); 
-    volatile int * dist_1 = (int *) (LW_virtual + DIST_SENSOR_1); 
+    volatile int * right_servo_encoder = (int *) (LW_virtual + RIGHT_SERVO_ENCODER);
+    volatile int * left_servo_encoder = (int *) (LW_virtual + LEFT_SERVO_ENCODER);
+    volatile int * dist_1 = (int *) (LW_virtual + DIST_SENSOR_1);
     volatile int * dist_2 = (int *) (LW_virtual + DIST_SENSOR_2);
-	volatile int * push_button = (int *) (LW_virtual + 0x00000050); 
-	
-	while(1){
+	volatile int * push_button = (int *) (LW_virtual + 0x00000050);
+
+    struct Dist_sensor sensors;
+
+    sensors.sideways_sensor = dist_1;
+    sensors.backwards_sensor = dist_2;
+
+    pthread_t thread1;
+    printf("Starting thread\n");
+    pthread_create(&thread1, NULL, sensor_thread, (void*) &sensors);
+    while(1){
         if(*push_button){
             break;
         }
     }
-
-    
-    // turn_right(left_servo_encoder, right_servo_encoder,left_servo, right_servo);
-     while(1){
+    while(1){
          drive_straight(35, left_servo, right_servo, left_servo_encoder, right_servo_encoder);
+         if(query_weighted_distances(2) > 50)
+            break;
     }
+    write_servo(0, left_servo, 1);
+    write_servo(0, right_servo, 0);
+    printf("Killing Thread\n");
+    killWhile();
+    if(pthread_join(thread1, NULL)) {
+        fprintf(stderr, "Error joining thread\n");
+        return 2;
+    }
+
     //testSpeeds(left_servo, right_servo, push_button);
-    // turn_right(left_servo_encoder, right_servo_encoder,left_servo, right_servo);
 //STARTING TO COMMENT OUT HERE
     /* Code to make it move forward
     TODO: place into function
     write_servo(0, right_servo);
     write_servo(0, left_servo);
-   
+
     Reads distance sensor
     TODO: place into function.
     while(1){
@@ -154,7 +161,7 @@ int main(void)
         sleep(1);
     }
     ENDING COMMENT HERE */
-   
+
 
     /*// THREAD STUFF
 
@@ -163,7 +170,7 @@ int main(void)
     //    pthread_attr_t *attr: attributes to apply to this thread
     //    void *(*start_routine)(void *): the function this thread executes
     //    void *arg: arguments to pass to thread function above
-    
+
     //NOTE: when compiling: gcc main_boy.c -o main_boy -lpthread
     printf("calling 1\n");
     //creating thread 1
@@ -196,13 +203,13 @@ int main(void)
         return 2;
 
     }*/
-    
+
     // Unmap FPGA bridge
     unmap_physical (LW_virtual, LW_BRIDGE_SPAN);
     close_physical (fd);
     return 0;
 
-   
+
 }
 
 /* #1 Possible Routing w/ 1 angled blade pointing right (for example)
@@ -220,9 +227,9 @@ int main(void)
 	|			|	|						|
 	|			|	|						|
 	|			|	|						|
-	|			|	|						|			
+	|			|	|						|
 	|			>------^					|
 	-----------------------------------------
 	Basically making a spiral out from a middle center line.
 	Always turning the opposite direction of the direction of the blade.
-*/ 
+*/
